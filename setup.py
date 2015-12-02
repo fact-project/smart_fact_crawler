897abc2d0aa5767ef2b336305f47fa04d9ed26a2
sebastian <sebmuell@phys.ethz.ch>
2015-12-02 18:34:36 +0100

    setup for easy install

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup, Extension
+
+setup(
+    name='smart_fact_crawler',
+    version='0.0.1',
+    description='acquieres data published on the smartfact web page',
+    url='https://bitbucket.org/Relleums/smartfactcrawler',
+    author='Dominik Neise, Sebastian Mueller',
+    author_email='sebmuell@phys.ethz.ch',
+    license='MIT',
+    packages=[
+        'smart_fact_crawler',
+        ],
+    install_requires=[
+        'requests',
+        'python-dateutil',
+    ],
+    zip_safe=False
+)
