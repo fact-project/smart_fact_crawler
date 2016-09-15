08eee32e5c4155eec9493673e3b7ff924ae26ce2
sebastian <sebmuell@phys.ethz.ch>
2016-09-15 12:25:09 +0200

    remove else from try except block and order member functions in table crawler based on their first occurence

--- smart_fact_crawler/__init__.py
+++ smart_fact_crawler/__init__.py
@@ -18,37 +18,24 @@ class TableCrawler(object):
         self.connection_error_counter = 0
         self._load_payload()
 
-    def __getitem__(self, index):
-        row, col = index
-        while True:
-            try:
-                item = self.page_payload[row][col]
-            except IndexError:
-                self._acknowledge_error_and_wait_a_moment()
-            else:
-                self.connection_error_counter = 0
-                break
-        return item
+    def _load_payload(self):
+        self._request_web_page()
+        self._build_page_payload()
 
     def _request_web_page(self):
         while True:
             try:
                 self.web_page = requests.get(self.url, timeout=15.)
-            except requests.exceptions.ConnectionError:
-                self._acknowledge_error_and_wait_a_moment()
-            else:
                 self.connection_error_counter = 0
                 break
+            except requests.exceptions.ConnectionError:
+                self._acknowledge_error_and_wait_a_moment()
 
     def _build_page_payload(self):
         self.page_payload = [
             [html.unescape(elem) for elem in line.split('\t')]
             for line in self.web_page.text.splitlines()
         ]
-
-    def _load_payload(self):
-        self._request_web_page()
-        self._build_page_payload()
 
     def _acknowledge_error_and_wait_a_moment(self):
         self.connection_error_counter += 1
@@ -57,6 +44,18 @@ class TableCrawler(object):
         else:
             # sleep between 1 and 2 seconds.
             time.sleep(1. + random.random())        
+
+    def __getitem__(self, index):
+        row, col = index
+        while True:
+            try:
+                item = self.page_payload[row][col]
+                self.connection_error_counter = 0
+                break
+            except IndexError:
+                self._acknowledge_error_and_wait_a_moment()
+        return item
+
 
 def str2float(text):
     try:
