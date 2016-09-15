bff8d501ce2f92b15703a9eac3157300939c52ae
Dominik Neise <neised@phys.ethz.ch>
2016-09-15 21:17:49 +0200

    finish new api

--- smart_fact_crawler/__init__.py
+++ smart_fact_crawler/__init__.py
@@ -13,7 +13,7 @@ def to_namedtuple(name, dictionary):
 
 smartfacturl = "http://fact-project.org/smartfact/data/"
 
-def all(self):
+def all():
     functions = [
         status,
         drive,
@@ -28,7 +28,9 @@ def all(self):
         main_page,
         trigger_rate,
         errorhist]
-    return {f.__name__: f() for f in functions}
+    return to_namedtuple('SmartFact',
+        {f.__name__: f() for f in functions}
+    )
 
 
 def drive():
@@ -54,22 +56,22 @@ def tracking(url=smartfacturl + 'tracking.data'):
 
 def pointing(url=smartfacturl + 'pointing.data'):
     tc = TableCrawler(url)
-    return {
-        'timestamp': sft2dt(tc[0, 0]),
-        'Azimuth_in_Deg': s2f(tc[1, 1]),
-        'Zenith_Distance_in_Deg': s2f(tc[2, 1]),
-    }
+    return to_namedtuple('PointingPage', {
+        'timestamp': (sft2dt(tc[0, 0]), None),
+        'azimuth': (s2f(tc[1, 1]), 'deg'),
+        'zenith_distance': (s2f(tc[2, 1]), 'deg'),
+    })
 
 
 def sqm(url=smartfacturl + 'sqm.data'):
     tc = TableCrawler(url)
-    return {
-        'timestamp': sft2dt(tc[0, 0]),
-        'Magnitude': s2f(tc[1, 1]),
-        'Sensor_Frequency_in_Hz': s2f(tc[2, 1]),
-        'Sensor_Period_in_s': s2f(tc[4, 1]),
-        'Sensor_Temperature_in_C': s2f(tc[5, 1]),
-    }
+    return to_namedtuple('SqmPage', {
+        'timestamp': (sft2dt(tc[0, 0]), None),
+        'magnitude': (s2f(tc[1, 1]), '???'),
+        'sensor_frequency': (s2f(tc[2, 1]), 'Hz'),
+        'sensor_period': (s2f(tc[4, 1]), 's'),
+        'sensor_temperature': (s2f(tc[5, 1]), 'C'),
+    })
 
 
 def sun(url=smartfacturl + 'sun.data'):
@@ -84,154 +86,154 @@ def sun(url=smartfacturl + 'sun.data'):
             new_date += timedelta(days=1)
         return new_date
 
+    conv = next_datetime_from_hhmm_string
 
     tc = TableCrawler(url)
-    return {
-        'timestamp': sft2dt(tc[0, 0]),
-        'End_of_dark_time':         next_datetime_from_hhmm_string(tc[1, 1]),
-        'End_of_astro_twilight':    next_datetime_from_hhmm_string(tc[2, 1]),
-        'End_of_nautic_twilight':   next_datetime_from_hhmm_string(tc[3, 1]),
-        'Start_of_day_time':        next_datetime_from_hhmm_string(tc[4, 1]),
-        'End_of_day_time':          next_datetime_from_hhmm_string(tc[5, 1]),
-        'Start_of_nautic_twilight': next_datetime_from_hhmm_string(tc[6, 1]),
-        'Start_of_astro_twilight':  next_datetime_from_hhmm_string(tc[7, 1]),
-        'Start_of_dark_time':       next_datetime_from_hhmm_string(tc[8, 1]),
-    }
+    return to_namedtuple('SunPage', {
+        'timestamp': (sft2dt(tc[0, 0]), None),
+        'End_of_dark_time': (conv(tc[1, 1]), None),
+        'End_of_astro_twilight': (conv(tc[2, 1]), None),
+        'End_of_nautic_twilight': (conv(tc[3, 1]), None),
+        'Start_of_day_time': (conv(tc[4, 1]), None),
+        'End_of_day_time': (conv(tc[5, 1]), None),
+        'Start_of_nautic_twilight': (conv(tc[6, 1]), None),
+        'Start_of_astro_twilight': (conv(tc[7, 1]), None),
+        'Start_of_dark_time': (conv(tc[8, 1]), None),
+    })
 
 
 def weather(url=smartfacturl + 'weather.data'):
     tc = TableCrawler(url)
-    return {
-        'timestamp': sft2dt(tc[0, 0]),
-        'Sun_in_Percent': tc[1, 1],
-        'Moon_in_Percent': tc[2, 1],
-        'Temperature_in_C': s2f(tc[3, 1]),
-        'Dew_point_in_C': s2f(tc[4, 1]),
-        'Humidity_in_Percent': s2f(tc[5, 1]),
-        'Pressure_in_hPa': s2f(tc[6, 1]),
-        'Wind_speed_in_km_per_h': s2f(tc[7, 1]),
-        'Wind_gusts_in_km_per_h': s2f(tc[8, 1]),
-        'Wind_direction': tc[9, 1],
-        'Dust_TNG_in_ug_per_m3': s2f(tc[10, 1]),
-    }
+    return to_namedtuple('WeatherPage', {
+        'timestamp': (sft2dt(tc[0, 0]), None),
+        'sun': (tc[1, 1], '%'),
+        'moon': (tc[2, 1], '%'),
+        'temperature': (s2f(tc[3, 1]), 'C'),
+        'dew_point': (s2f(tc[4, 1]), 'C'),
+        'humidity': (s2f(tc[5, 1]), '%'),
+        'pressure': (s2f(tc[6, 1]), 'hPa'),
+        'sind_speed': (s2f(tc[7, 1]), 'km/h'),
+        'wind_gusts': (s2f(tc[8, 1]), 'km/h'),
+        'wind_direction': (tc[9, 1], None),
+        'dust_tng': (s2f(tc[10, 1]), 'ug/m3'),
+    })
 
 
 def sipm_currents(url=smartfacturl + 'current.data'):
     tc = TableCrawler(url)
-    return {
-        'timestamp': sft2dt(tc[0, 0]),
-        'Clibrated': tc[1, 1],
-        'Min_current_per_GAPD_in_uA': s2f(tc[2, 1]),
-        'Med_current_per_GAPD_in_uA': s2f(tc[3, 1]),
-        'Avg_current_per_GAPD_in_uA': s2f(tc[4, 1]),
-        'Max_current_per_GAPD_in_uA': s2f(tc[5, 1]),
-        # The W is stucked to the float and needs to be removed
-        'Power_camera_GAPD_in_W': s2f(tc[6, 1][:-1]),
-    }
+    return to_namedtuple('CurrentPage', {
+        'timestamp': (sft2dt(tc[0, 0]), None),
+        'calibrated': (tc[1, 1], None),
+        'min_per_sipm': (s2f(tc[2, 1]), 'uA'),
+        'median_per_sipm': (s2f(tc[3, 1]), 'uA'),
+        'mean_per_sipm': (s2f(tc[4, 1]), 'uA'),
+        'max_per_sipm': (s2f(tc[5, 1]), 'uA'),
+        'power': (s2f(tc[6, 1][:-1]), 'W'),
+    })
 
 def sipm_voltages(url=smartfacturl + 'voltage.data'):
     tc = TableCrawler(url)
-    return {
-        'timestamp': sft2dt(tc[0, 0]),
-        'Min_voltage_in_V': s2f(tc[1, 1]),
-        'Med_voltage_in_V': s2f(tc[2, 1]),
-        'Avg_voltage_in_V': s2f(tc[3, 1]),
-        'Max_voltage_in_V': s2f(tc[4, 1]),
-    }
+    return to_namedtuple('VoltagePage', {
+        'timestamp': (sft2dt(tc[0, 0]), None),
+        'min': (s2f(tc[1, 1]), 'V'),
+        'median': (s2f(tc[2, 1]), 'V'),
+        'mean': (s2f(tc[3, 1]), 'V'),
+        'max': (s2f(tc[4, 1]), 'V'),
+    })
 
 def status(url=smartfacturl + 'status.data'):
     tc = TableCrawler(url)
-    return {
-        'timestamp': sft2dt(tc[0, 0]),
-        'DIM': tc[1, 1],
-        'Dim_Control': tc[2, 1],
-        'MCP': tc[3, 1],
-        'Datalogger': tc[4, 1],
-        'Drive_control': tc[5, 1],
-        'Drive_PC_time_check': tc[6, 1],
-        'FAD_control': tc[7, 1],
-        'FTM_control': tc[8, 1],
-        'Bias_control': tc[9, 1],
-        'Feedback': tc[10, 1],
-        'Rate_control': tc[11, 1],
-        'FSC_control': tc[12, 1],
-        'PFmini_control': tc[13, 1],
-        'GPS_control': tc[14, 1],
-        'SQM_control': tc[15, 1],
-        'Agilent_control_24V': tc[16, 1],
-        'Agilent_control_50V': tc[17, 1],
-        'Agilent_control_80V': tc[18, 1],
-        'Power_control': tc[19, 1],
-        'Lid_control': tc[20, 1],
-        'Ratescan': tc[21, 1],
-        'Magic_Weather': tc[22, 1],
-        'TNG_Weather': tc[23, 1],
-        'Magic_Lidar': tc[24, 1],
-        'Temperature': tc[25, 1],
-        'Chat_server': tc[26, 1],
-        'Skype_client': tc[27, 1],
-        'Free_space_newdaq_in_TB': s2f(tc[28, 1]),
-        'Free_space_daq_in_TB': s2f(tc[29, 1]),
-        'Smartfact_runtime': tc[30, 1],
-    }
+    return to_namedtuple('StatusPage', {
+        'timestamp': (sft2dt(tc[0, 0]), None),
+        'dim': (tc[1, 1], None),
+        'dim_control': (tc[2, 1], None),
+        'mcp': (tc[3, 1], None),
+        'datalogger': (tc[4, 1], None),
+        'drive_control': (tc[5, 1], None),
+        'drive_pc_time_check': (tc[6, 1], None),
+        'fad_control': (tc[7, 1], None),
+        'ftm_control': (tc[8, 1], None),
+        'bias_control': (tc[9, 1], None),
+        'feedback': (tc[10, 1], None),
+        'rate_control': (tc[11, 1], None),
+        'fsc_control': (tc[12, 1], None),
+        'pfmini_control': (tc[13, 1], None),
+        'gps_control': (tc[14, 1], None),
+        'sqm_control': (tc[15, 1], None),
+        'agilent_control_24v': (tc[16, 1], None),
+        'agilent_control_50v': (tc[17, 1], None),
+        'agilent_control_80v': (tc[18, 1], None),
+        'power_control': (tc[19, 1], None),
+        'lid_control': (tc[20, 1], None),
+        'ratescan': (tc[21, 1], None),
+        'magic_weather': (tc[22, 1], None),
+        'tng_weather': (tc[23, 1], None),
+        'magic_lidar': (tc[24, 1], None),
+        'temperature': (tc[25, 1], None),
+        'chat_server': (tc[26, 1], None),
+        'skype_client': (tc[27, 1], None),
+        'free_space_newdaq': (s2f(tc[28, 1]), 'TB'),
+        'free_space_daq': (s2f(tc[29, 1]), 'TB'),
+        'smartfact_runtime': (tc[30, 1], None),
+    })
 
 
 def container_temperature(url=smartfacturl + 'temperature.data'):
     tc = TableCrawler(url)
-    return {
-        'timestamp': sft2dt(tc[0, 0]),
-        '24h_min_temperature_in_C': tc[1, 1],
-        'Current_temperature_in_C': tc[2, 1],
-        '24h_max_temperature_in_C': tc[3, 1],
-    }
+    return to_namedtuple('ContainerTemperaturePage', {
+        'timestamp': (sft2dt(tc[0, 0]), None),
+        'daily_min': (tc[1, 1], 'C'),
+        'current': (tc[2, 1], 'C'),
+        'daily_max': (tc[3, 1], 'C'),
+    })
 
 
 def current_source(url=smartfacturl + 'source.data'):
     tc = TableCrawler(url)
-    return {
-        'timestamp': sft2dt(tc[0, 0]),
-        'Source_Name': tc[1, 1:],
-        'Right_Ascention_in_h': s2f(tc[2, 1]),
-        'Declination_in_Deg': s2f(tc[3, 1]),
-        'Wobble_offset_in_Deg': s2f(tc[4, 1]),
-        'Wobble_angle_in_Deg': s2f(tc[5, 1]),
-    }
+    return to_namedtuple('SourcePage', {
+        'timestamp': (sft2dt(tc[0, 0]), None),
+        'name': (tc[1, 1:], None),
+        'right_ascension': (s2f(tc[2, 1]), 'h'),
+        'declination': (s2f(tc[3, 1]), 'deg'),
+        'wobble_offset': (s2f(tc[4, 1]), 'deg'),
+        'wobble_angle': (s2f(tc[5, 1]), 'deg'),
+    })
 
 
 def camera_climate(url=smartfacturl + 'fsc.data'):
     tc = TableCrawler(url)
-    return {
-        'timestamp': sft2dt(tc[0, 0]),
-        'Avg_humidity_in_percent': s2f(tc[1, 1]),
-        'Max_rel_temp_in_C': s2f(tc[2, 1]),
-        'Avg_rel_temp_in_C': s2f(tc[3, 1]),
-        'Min_rel_temp_in_C': s2f(tc[4, 1]),
-    }
+    return to_namedtuple('CameraClimatePage', {
+        'timestamp': (sft2dt(tc[0, 0]), None),
+        'humidity_mean': (s2f(tc[1, 1]), '%'),
+        'relative_temperature_max': (s2f(tc[2, 1]), 'C'),
+        'relative_temperature_mean': (s2f(tc[3, 1]), 'C'),
+        'relative_temperature_min': (s2f(tc[4, 1]), 'C'),
+    })
 
 
 def main_page(url=smartfacturl + 'fact.data'):
     tc = TableCrawler(url)
-    return {
-        'Time_Stamp_1': sft2dt(tc[0, 0]),
-        'Time_Stamp_2': sft2dt(tc[0, 1]),
-        'System_Status': ' '.join(tc[1, 1:]),
-        'Rel_camera_temp_in_C': s2f(tc[3, 1]),
-        'Humidity_in_Percent': s2f(tc[4, 1]),
-        'Wind_speed_in_km_per_h': s2f(tc[4, 2]),
-    }
+    return to_namedtuple('MainPage', {
+        'timestamp_1': (sft2dt(tc[0, 0]), None),
+        'timestamp_2': (sft2dt(tc[0, 1]), None),
+        'system_status': (' '.join(tc[1, 1:]), None),
+        'relative_camera_temperature': (s2f(tc[3, 1]), 'C'),
+        'humidity': (s2f(tc[4, 1]), '%'),
+        'wind_speed': (s2f(tc[4, 2]), 'km/h'),
+    })
 
 
 def trigger_rate(url=smartfacturl + 'trigger.data'):
     tc = TableCrawler(url)
-    return {
-        'timestamp': sft2dt(tc[0, 0]),
-        'Trigger_Rate_in_1_per_s': s2f(tc[1, 1]),
-    }
+    return to_namedtuple('TriggerPage', {
+        'timestamp': (sft2dt(tc[0, 0]), None),
+        'trigger_rate': (s2f(tc[1, 1]), '1/s'),
+    })
 
 def errorhist(url=smartfacturl + 'errorhist.data'):
     tc = TableCrawler(url)
     history = [h for h in tc[1, 1].split("<->")[1].split("<br/>") if len(h)]
-    return {
-        'timestamp': sft2dt(tc[0, 0]),
-        'history': history,
-    }
+    return to_namedtuple('ErrorHistPage', {
+        'timestamp': (sft2dt(tc[0, 0]), None),
+        'history': (history, None),
+    })
