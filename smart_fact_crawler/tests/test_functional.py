da1ec891e3b8496d7ae70da939d25c0ac7c7c592
Dominik Neise <neised@phys.ethz.ch>
2016-09-29 13:00:12 +0200

    reverting back

--- smart_fact_crawler/tests/test_functional.py
+++ smart_fact_crawler/tests/test_functional.py
@@ -1,47 +1,19 @@
 from os import path
+import smart_fact_crawler as sfc
 from datetime import datetime
-
-from pytest import raises
-import pytest
-
-@pytest.fixture
-def sfc():
-    import smart_fact_crawler as sfc
-    dir_ = path.join(
-        path.dirname(sfc.__file__),
-        'resources',
-        '20160703_233149',
-        )
-    assert path.isdir(dir_)
-    sfc.smartfacturl = 'file:' + dir_
-    return sfc
-
-@pytest.fixture
-def broken_fsc():
-    import smart_fact_crawler as sfc
-    dir_ = path.join(
-        path.dirname(sfc.__file__),
-        'resources',
-        '20160703_233149_broken_fsc',
-        )
-    assert path.isdir(dir_)
-    sfc.smartfacturl = 'file:' + dir_
-    return sfc
+from pprint import pprint
 
 def test_is_install_folder_a_directory():
-    import smart_fact_crawler as sfc
     dir_ = path.dirname(sfc.__file__)
     assert path.isdir(dir_)
 
 def test_can_find_resource_folder():
-    import smart_fact_crawler as sfc
     dir_ = path.join(
         path.dirname(sfc.__file__),
         'resources')
     assert path.isdir(dir_)
 
 def test_can_find_a_testfilefolder():
-    import smart_fact_crawler as sfc
     dir_ = path.join(
         path.dirname(sfc.__file__),
         'resources',
@@ -49,11 +21,21 @@ def test_can_find_a_testfilefolder():
         )
     assert path.isdir(dir_)
 
-def test_smartfact(sfc):
+def test_smartfact():
+    sfc.smartfacturl = 'file:' + path.join(
+        path.dirname(sfc.__file__),
+        'resources',
+        '20160703_233149',
+        )
     
     sfc.smartfact()
 
-def test_timestamp_dates(sfc):
+def test_timestamp_dates():
+    sfc.smartfacturl = 'file:' + path.join(
+        path.dirname(sfc.__file__),
+        'resources',
+        '20160703_233149',
+        )
     test_date = datetime(2016, 7, 3).date()
     
     complete = sfc.smartfact()
@@ -65,12 +47,23 @@ def test_timestamp_dates(sfc):
             if 'timestamp' in row_name:
                 assert row.date() == test_date
 
+from pytest import raises
+def test_broken_page():
+    sfc.smartfacturl = 'file:' + path.join(
+        path.dirname(sfc.__file__),
+        'resources',
+        '20160703_233149_broken_fsc',
+        )
 
-def test_broken_page(broken_fsc):
     with raises(IndexError):
-        broken_fsc.camera_climate()
+        sfc.camera_climate()
 
-def test_source_name(sfc):
+def test_source_name():
+    sfc.smartfacturl = 'file:' + path.join(
+        path.dirname(sfc.__file__),
+        'resources',
+        '20160703_233149',
+        )
 
     assert sfc.current_source().name == 'Mrk 501'
     assert sfc.drive_tracking().source_name == 'Mrk 501'
