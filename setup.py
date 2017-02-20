e84263ce17563fd7c888ab23fe105a9bd29a86a7
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-02-20 16:49:14 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='smart_fact_crawler',
-    version='0.2.2',
+    version='0.3.0',
     description='acquieres data published on the smartfact web page',
     url='https://github.com/fact-project/smart_fact_crawler.git',
     author='Dominik Neise, Sebastian Mueller, Maximilian Nöthe',
