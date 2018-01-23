6f61383cc3388248434635dff3bd486ab59ba3b3
Dominik Neise <neised@phys.ethz.ch>
2018-01-23 09:49:54 +0100

    fixed it

--- download_smartfact_data.py
+++ download_smartfact_data.py
@@ -10,7 +10,7 @@ from functools import partial
 logging.basicConfig(level=logging.INFO)
 
 
-url = 'http://fact-project.org/smartfact/data/{}.data'
+url = 'http://fact-project.org/smartfact/data/{}'
 files = [
     'agilent24.data',
     'agilent50.data',
@@ -107,9 +107,7 @@ def make_output_dir():
 
 
 def make_out_path(output_directory, filename):
-    os.path.join(
-        output_directory, '{}.data'.format(filename)
-    )
+    return os.path.join(output_directory, filename)
 
 
 def download_all():
@@ -126,19 +124,10 @@ def download_all():
             )
 
 
-def download_all_without_executor():
-    logging.info('Start download_all_without_executor')
-    output_directory = make_output_dir()
-    out_path = partial(make_out_path, output_directory)
-
-    for filename in files:
-        download(
-            url.format(filename),
-            out_path(filename)
-        )
-
 if __name__ == '__main__':
-    while True:
-        download_all_without_executor()
-        sleep(5 * 60)
+    download_all()
 
+    schedule.every(5).minutes.do(download_all)
+    while True:
+        schedule.run_pending()
+        sleep(10)
