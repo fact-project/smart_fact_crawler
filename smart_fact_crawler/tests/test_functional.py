6dab9e8fd10307abcbe23fce6ea15b903b5e34b9
Dominik Neise <neised@phys.ethz.ch>
2018-01-24 12:27:21 +0100

    Revert "create a test, asserting the file contents are in the exception"
    This reverts commit e21a4ca326b2b6e5496d615ae3e84238af2f45b8.

--- smart_fact_crawler/tests/test_functional.py
+++ smart_fact_crawler/tests/test_functional.py
@@ -54,23 +54,14 @@ def test_timestamp_dates():
 
 
 def test_broken_page():
-    test_file_dir = path.join(
+    sfc.smartfacturl = 'file:' + path.join(
         path.dirname(sfc.__file__),
         'resources',
         '20160703_233149_broken_fsc',
         )
-    test_file_content = open(
-        path.join(test_file_dir, 'fsc.data')
-        ).read()
-    sfc.smartfacturl = 'file:' + test_file_dir
 
-    with raises(IndexError) as excinfo:
+    with raises(IndexError):
         sfc.camera_climate()
-
-    message = excinfo.value.args[0]
-    print(message)
-    assert "text:" in message
-    assert test_file_content in message
 
 
 def test_broken_page_fallback():
