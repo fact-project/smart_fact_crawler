3fa4030df3c9936e35763796c62e4544789bb891
Kai Brügge <kai.bruegge@tu-dortmund.de>
2018-01-25 18:14:38 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='smart_fact_crawler',
     version='0.4.1',
-    description='acquieres data published on the smartfact web page',
+    description='acquire data published on the smartfact web page',
     url='https://github.com/fact-project/smart_fact_crawler.git',
     author='Dominik Neise, Sebastian Mueller, Maximilian Nöthe',
     author_email='sebmuell@phys.ethz.ch',
